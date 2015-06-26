f2fcbf9a0acb39af8e05da0f934c404606332dc3
amitabhasaha1987 <sahaamitabha123@gmail.com>
2015-06-27 01:53:41 +0530

    Data Access Layer

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base("StylisticsAdmin")
+        {
+        }
+
+        public virtual DbSet<Merchant> Merchants { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Merchant>()
+               .Property(e => e.merchant_name)
+               .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.incharge_name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.url)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.contact_email)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.contact_phone)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.contact_fax)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.username)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.password)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Merchant>()
+                .Property(e => e.logo)
+                .IsUnicode(false);
+        }
+
+       
+        
+       
+    }
+}
---
+++ DataAccess/Models/Merchant.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace DataAccess.Models
+{
+    [Table("Merchant")]
+    public partial class Merchant
+    {
+
+        [Key]
+        public long id { get; set; }
+
+        public UserType super_user { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string merchant_name { get; set; }
+
+        [StringLength(50)]
+        public string incharge_name { get; set; }
+
+        [StringLength(255)]
+        public string url { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string contact_email { get; set; }
+
+        [StringLength(20)]
+        public string contact_phone { get; set; }
+
+        [StringLength(20)]
+        public string contact_fax { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string password { get; set; }
+
+        [StringLength(150)]
+        public string logo { get; set; }
+
+        public DateTime created { get; set; }
+
+        public DateTime? modified { get; set; }
+
+        public short? status { get; set; }
+
+        public short is_synched { get; set; }
+    }
+
+    public enum UserType : byte 
+    {
+        admin = 1,
+        Merchant = 0
+    }
+}
