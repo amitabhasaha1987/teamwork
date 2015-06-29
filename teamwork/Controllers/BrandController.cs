f8d297945e6cfeacd482f747d2213cb50224b1c1
amitabhasaha1987 <sahaamitabha123@gmail.com>
2015-06-29 21:30:47 +0530

    Brand & Tag Controller

---
+++ teamwork/Controllers/BrandController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess.Models;
+using DataAccess;
+
+namespace teamwork.Controllers
+{
+    [AuthorizeAccess(UserType.admin)]
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Brand brand)
+        {
+            if(ModelState.IsValid)
+            {
+                brand.created = DateTime.Now;
+                brand.is_synched = 1;
+
+                using(Context db = new Context())
+                {
+                    db.Brands.Add(brand);
+                    db.SaveChanges();
+                    TempData["Status"] = "1";
+                    return RedirectToAction("Create");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ teamwork/Controllers/TagController.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace teamwork.Controllers
+{
+    [AuthorizeAccess(UserType.admin)]
+    public class TagController : Controller
+    {
+        // GET: Tag
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                tag.created = DateTime.Now;
+                tag.is_synched = 1;
+
+                using (Context db = new Context())
+                {
+                    db.Tags.Add(tag);
+                    db.SaveChanges();
+                    TempData["Status"] = "1";
+                    return RedirectToAction("Create");
+                }
+            }
+            return View();
+        }
+    }
+}
